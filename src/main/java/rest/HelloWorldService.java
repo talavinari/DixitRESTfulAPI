eb870d9bd8a704d638ef5481e9a70549e1c45c38
tal <talavinari@gmail.com>
2016-03-08 20:16:50 +0200

    first commit with db config

--- src/main/java/rest/HelloWorldService.java
+++ src/main/java/rest/HelloWorldService.java
@@ -1,10 +1,17 @@
 package rest;
 
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Path("/hello")
 public class HelloWorldService {
@@ -12,7 +19,26 @@ public class HelloWorldService {
     @GET
     @Path("/{param}")
     public Response getMsg(@PathParam("param") String msg) {
-        String output = "Tal say : " + msg;
+        Context initCtx = null;
+        String value = "Empty";
+        try {
+            initCtx = new InitialContext();
+            Context envCtx = null;
+            envCtx = (Context) initCtx.lookup("java:comp/env");
+            DataSource ds = null;
+            ds = (DataSource)
+                    envCtx.lookup("jdbc/test");
+            ResultSet rs = ds.getConnection().createStatement().executeQuery("select message from tal");
+            while (rs.next()) {
+                value = rs.getString("MESSAGE");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+        String output = "Tal say : " + value;
         return Response.status(200).entity(output).build();
     }
 }
