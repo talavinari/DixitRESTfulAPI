0479461ee4513a6730dbd18d045e8c7f72598eaa
tal <talavinari@gmail.com>
2016-03-12 23:23:46 +0200

    rest continue

--- src/main/java/rest/DixitConfigurationService.java
+++ src/main/java/rest/DixitConfigurationService.java
@@ -16,39 +16,38 @@ import java.util.List;
 @Path("/service")
 public class DixitConfigurationService {
     public static final String ROOM_NAME_COLUMN = "name";
-    public static final String PLAYER_NAME_COLUMN = "name";
-    public static final String GET_PLAYERS_IN_ROOM_QUERY = "select p." + PLAYER_NAME_COLUMN +
-                                                            " from players_to_rooms ptr, players p where ptr.player_id = p.id " +
-                                                            " AND room_id = ?";
+    public static final String GET_ROOMS_QUERY = "select " + ROOM_NAME_COLUMN + " from rooms";
+    public static final String PLAYER_NAME_COLUMN = "player_name";
+    public static final String GET_PLAYERS_IN_ROOM_QUERY = "SELECT " + PLAYER_NAME_COLUMN +
+                                                            " FROM players_to_rooms WHERE " +
+                                                            " room_id = ?";
     public static final String GET_ROOM_ID_QUERY = "select id from rooms where " + ROOM_NAME_COLUMN + " = ?";
+    public static final String JOIN_USER_QUERY = "insert into players_to_rooms (select ?, ?,  " +
+                                                 "max(player_index) + 1  from players_to_rooms where room_id = 1)";
+    public static final String NEW_ROOM_QUERY = "insert into rooms (name) values (?)";
 
     @POST
-    @Consumes("application/json")
-    @Path("try/")
-    public void joinRoom(DTO example) {
-        String s = example.name;
-    }
-
-    private int getRoomIdByName(String roomName) throws SQLException , NamingException {
-        PreparedStatement preparedStatement = getConnection().prepareStatement(GET_ROOM_ID_QUERY);
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("addRoom/")
+    public void addRoom(String roomName) {
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(NEW_ROOM_QUERY);
             preparedStatement.setString(1, roomName);
-            ResultSet rs = preparedStatement.executeQuery();
-            int roomId = -1;
-            while (rs.next()) {
-                roomId = rs.getInt("id");
-            }
-
-        return roomId;
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
 
     @POST
-    @Consumes("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("join/")
     public void joinRoom(JoinRequestDTO joinRequestDTO) {
-        String insertSql = "insert into users_to_rooms values (?, ?)";
         try {
             int roomId = getRoomIdByName(joinRequestDTO.roomName);
-            PreparedStatement preparedStatement = getConnection().prepareStatement(insertSql);
+            PreparedStatement preparedStatement = getConnection().prepareStatement(JOIN_USER_QUERY);
             preparedStatement.setInt(1, roomId);
             preparedStatement.setString(2, joinRequestDTO.nickName);
             preparedStatement.execute();
@@ -61,8 +60,8 @@ public class DixitConfigurationService {
 
     @GET
     @Path("/players/{roomName}")
-    @Produces({ MediaType.APPLICATION_XML})
-    public ResponseList getPlayersInRoom(@PathParam("roomName") String roomName) {
+    @Produces({ MediaType.APPLICATION_JSON})
+    public List<String> getPlayersInRoom(@PathParam("roomName") String roomName) {
         List<String> players = new ArrayList<String>();
         try {
             Connection con = getConnection();
@@ -80,20 +79,18 @@ public class DixitConfigurationService {
             e.printStackTrace();
         }
 
-        ResponseList responseList = new ResponseList();
-        responseList.setList(players);
-        return responseList;
+        return players;
     }
 
     @GET
     @Path("/rooms")
-    @Produces({ MediaType.APPLICATION_XML})
-    public ResponseList getRooms() {
+    @Produces({ MediaType.APPLICATION_JSON})
+    public List<String> getRooms() {
 
         List<String> allRooms = new ArrayList<String>();
         try {
             Connection con = getConnection();
-            ResultSet rs = con.createStatement().executeQuery("select " + ROOM_NAME_COLUMN + " from rooms");
+            ResultSet rs = con.createStatement().executeQuery(GET_ROOMS_QUERY);
             while (rs.next()) {
                  allRooms.add(rs.getString(ROOM_NAME_COLUMN));
             }
@@ -104,9 +101,7 @@ public class DixitConfigurationService {
             e.printStackTrace();
         }
 
-        ResponseList responseList = new ResponseList();
-        responseList.setList(allRooms);
-        return responseList;
+        return allRooms;
 //        return Response.status(200).entity(allRooms).build();
     }
 
@@ -115,5 +110,17 @@ public class DixitConfigurationService {
         Context envCtx = (Context) initCtx.lookup("java:comp/env");
         DataSource ds = (DataSource) envCtx.lookup("jdbc/test");
         return ds.getConnection();
+    }
+
+    private int getRoomIdByName(String roomName) throws SQLException , NamingException {
+        PreparedStatement preparedStatement = getConnection().prepareStatement(GET_ROOM_ID_QUERY);
+        preparedStatement.setString(1, roomName);
+        ResultSet rs = preparedStatement.executeQuery();
+        int roomId = -1;
+        while (rs.next()) {
+            roomId = rs.getInt("id");
+        }
+
+        return roomId;
     }
 }
